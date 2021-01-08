ad78566fa65e42b81fb5ccc691bcca5a4a4d5417
Pedro Vaz <pedroskzt@gmail.com>
2021-01-08 18:14:17 -0300

    Acrescentado a funcionalidade de autocomplete no input search da navbar

--- assinaturas/views/visualizar_assinatura.py
+++ assinaturas/views/visualizar_assinatura.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_list_or_404
 from assinaturas.forms import AssinaturaForms
+from assinaturas.models import Assinatura
 
 
 def visualizar_assinatura(request):
@@ -7,9 +8,19 @@ def visualizar_assinatura(request):
         form = AssinaturaForms(request.POST)
         if form.is_valid():
             form.save()
-            contexto = {'form': form}
+            query = Assinatura.objects.filter(nome=form['nome'].value()).get()
+            contexto = {'assinatura': query}
             return render(request, 'visualizar_assinatura.html', contexto)
         else:
             contexto = {'form': form}
             return render(request, 'criar_assinatura.html', contexto)
+    if request.method == 'GET':
+        query = Assinatura.objects.filter(nome=request.GET['buscar']).get()
+        print(query.nome)
+        print(query.departamento)
+        contexto = {'assinatura': query}
+        print(query)
+        return render(request, 'visualizar_assinatura.html', contexto)
     return redirect('criar_assinatura')
+
+
