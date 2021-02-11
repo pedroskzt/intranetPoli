52b5a6a97cbef300c6e6431f78ba1ac33a1836ea
GYN-CPD-PEDRO <pedroskzt@gmail.com>
2021-02-11 12:38:18 -0300

    Adicionado widget para facilitar a inserção de classes nos forms django

--- intranetPoli/settings.py
+++ intranetPoli/settings.py
@@ -52,6 +52,7 @@ ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     'Apps.subsTribut',
+    'widget_tweaks',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
