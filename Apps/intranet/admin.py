f66f90dcad0e1cdced6320e411acd8e64f7cb071
GYN-CPD-PEDRO <pedroskzt@gmail.com>
2021-05-07 18:57:07 -0300

    Resgitrado o Modelo dos LINKS no painel de adminsitração.

---
+++ Apps/intranet/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+
+from Apps.intranet.models.links import Links
+
+
+# Register your models here.
+
+class ListandoLinks(admin.ModelAdmin):
+    class Media:
+        css = {
+            'all': ('subsTribut/css/admin_changelists.css',)
+        }
+
+    list_display = ('titulo', 'url', 'data_ultima_alteracao',
+                    'usuario_ultima_alteracao', 'exibir')
+    list_editable = ('exibir',)
+    readonly_fields = ('usuario_criacao', 'usuario_ultima_alteracao')
+
+
+admin.site.register(Links, ListandoLinks)
