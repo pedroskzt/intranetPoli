78286bdc0895b0b5b96d6f70064f252a97e07b08
GYN-CPD-PEDRO <pedroskzt@gmail.com>
2021-05-07 18:48:18 -0300

    Criados novos URLS para as páginas.

---
+++ Apps/intranet/urls.py
@@ -0,0 +1,18 @@
+"""subsTribut URL Configuration
+
+"""
+from django.urls import path
+
+from Apps.intranet.views.gerenciar_intranet import painel_intranet, adicionar_link, editar_link, excluir_link
+from Apps.intranet.views.login import login, logout
+from Apps.intranet.views.pagina_inicial import index
+
+urlpatterns = [
+    path('', index, name='pagina_inicial'),
+    path('login', login, name='login'),
+    path('logout', logout, name='logout'),
+    path('painel/intranet', painel_intranet, name='painel_intranet'),
+    path('painel/intranet/adicionar', adicionar_link, name='adicionar_link'),
+    path('painel/intranet/editar/<int:link_id>', editar_link, name='editar_link'),
+    path('painel/intranet/excluir/<int:link_id>', excluir_link, name='excluir_link'),
+]
--- intranetPoli/urls.py
+++ intranetPoli/urls.py
@@ -1,13 +1,13 @@
 """intranetPoli URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
+The `urlpatterns` list routes URLs to blah. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
 Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
+Function blah
+    1. Add an import:  from my_app import blah
+    2. Add a URL to urlpatterns:  path('', blah.home, name='home')
+Class-based blah
+    1. Add an import:  from other_app.blah import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', include('Apps.subsTribut.urls')),
-    path('admin/', admin.site.urls),
-]
+    path('', include('Apps.intranet.urls')),
+    path('st', include('Apps.subsTribut.urls')),
+    path('painerl/cpd/', admin.site.urls, name='painel_cpd'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
