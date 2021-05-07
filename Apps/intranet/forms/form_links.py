e30d51c796159c26a893679f8104f4302da58e54
GYN-CPD-PEDRO <pedroskzt@gmail.com>
2021-05-07 18:43:19 -0300

    Criado Formulario para adicionar links a intranet.

---
+++ Apps/intranet/forms/form_links.py
@@ -0,0 +1,25 @@
+from django import forms
+
+from Apps.intranet.models.links import Links
+
+
+def _campo_vazio(valor_campo, nome_campo, lista_de_erros):
+    if not valor_campo.strip():
+        lista_de_erros[nome_campo] = f'Este campo não pode estar vazio ou em branco.'
+
+
+class NovoLinkForms(forms.ModelForm):
+    class Meta:
+        model = Links
+        fields = ('titulo', 'descricao', 'logo', 'url')
+        labels = {
+            'titulo': 'Título',
+            'descricao': 'Descrição',
+            'logo': 'Logo',
+            'url': 'Link da Página'
+        }
+        widgets = {
+            'titulo': forms.TextInput(attrs={'placeholder': 'Título'}),
+            'url': forms.TextInput(attrs={'placeholder': 'Link da Página'}),
+            'descricao': forms.Textarea(attrs={'placeholder': 'Descrição'}),
+        }
