02a2ff85fd5e04d970506174bbef02e1449ca680
GYN-CPD-PEDRO <pedroskzt@gmail.com>
2021-05-21 18:20:09 -0300

    Finalizado o CRUD do modelo Links: Adicionado metodo para atualização e metodo para exclusão.

--- Apps/intranet/views/gerenciar_intranet.py
+++ Apps/intranet/views/gerenciar_intranet.py
@@ -1,42 +1,55 @@
-from django.shortcuts import render, redirect
+import os
 
-from Apps.intranet.forms.form_links import NovoLinkForms
+from django.shortcuts import render, redirect, get_object_or_404
+
+from Apps.intranet.forms.form_links import NovoLinkForms, AtualizaLinkForms
 from Apps.intranet.models.links import Links
+from intranetPoli.settings import MEDIA_URL
 
-
-# TODO: Mostrar links no Dashboard de gerenciamento da intranet, mesmo marcado com Exibir=NÃO
 
 def painel_intranet(request):
-    links = Links.objects.filter(exibir=True)
+    links = Links.objects.all()
     return render(request, 'intranet/painel/painel_intranet.html', context={'links': links})
 
 
 def adicionar_link(request):
-    form = None
     if request.method == 'POST':
         form = NovoLinkForms(request.POST, request.FILES)
-        print('É POST')
         if request.user.is_authenticated and form.is_valid():
-            print("É valido")
             link = form.save(commit=False)
             link.usuario_criacao = request.user
             link.usuario_ultima_alteracao = request.user
             link.save()
-            # form.cleaned_data['USUARIO_CRIACAO_ID'] = get_object_or_404(User, pk=request.user.id)
-            # form.cleaned_data['USUARIO_ULTIMA_ALTERACAO_ID'] = get_object_or_404(User, pk=request.user.id)
-
             return redirect('painel_intranet')
-        else:
-            print('Form Invalido')
     else:
-        print('Não É POST')
         form = NovoLinkForms()
     return render(request, 'intranet/painel/adicionar_link.html', context={'forms': form})
 
 
 def editar_link(request, link_id):
-    return redirect('painel_intranet')
+    if request.method == 'GET':
+        link = get_object_or_404(Links, pk=link_id)
+        form = AtualizaLinkForms(instance=link)
+
+    elif request.method == 'POST':
+        link = get_object_or_404(Links, pk=link_id)
+        form = AtualizaLinkForms(request.POST, request.FILES, instance=link)
+        if request.user.is_authenticated and form.is_valid():
+            link = form.save(commit=False)
+            link.usuario_ultima_alteracao = request.user
+            link.save()
+            return redirect('painel_intranet')
+    contexto = {
+        'forms': form,
+        'link_id': link_id,
+        'media_url': MEDIA_URL
+    }
+    return render(request, 'intranet/painel/editar_link.html', context=contexto)
 
 
 def excluir_link(request, link_id):
+    # TODO: Adicionar modal de confirmação de exclusão
+    link = get_object_or_404(Links, pk=link_id)
+    os.remove(link.logo.path)
+    link.delete()
     return redirect('painel_intranet')
