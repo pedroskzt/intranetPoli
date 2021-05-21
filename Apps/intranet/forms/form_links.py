5b1002d835437643da4d9fcd49bbb33974e8cbb9
GYN-CPD-PEDRO <pedroskzt@gmail.com>
2021-05-21 18:17:24 -0300

    Adicionado Django Form para atualizar as instancia de Links

--- Apps/intranet/forms/form_links.py
+++ Apps/intranet/forms/form_links.py
@@ -23,3 +23,22 @@ class NovoLinkForms(forms.ModelForm):
             'url': forms.TextInput(attrs={'placeholder': 'Link da Página'}),
             'descricao': forms.Textarea(attrs={'placeholder': 'Descrição'}),
         }
+
+
+class AtualizaLinkForms(forms.ModelForm):
+    class Meta:
+        model = Links
+        fields = ('id', 'titulo', 'descricao', 'logo', 'url', 'exibir', 'requer_acesso')
+        labels = {
+            'titulo': 'Título',
+            'descricao': 'Descrição',
+            'logo': 'Logo',
+            'url': 'Link da Página',
+            'exibir': 'Exibir?',
+            'requer_acesso': 'Requer Acesso?'
+        }
+        widgets = {
+            'titulo': forms.TextInput(attrs={'placeholder': 'Título'}),
+            'url': forms.TextInput(attrs={'placeholder': 'Link da Página'}),
+            'descricao': forms.Textarea(attrs={'placeholder': 'Descrição'})
+        }
