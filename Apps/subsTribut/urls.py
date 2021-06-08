b339a6f68a9ebe170fabc28d629d97feffc101b0
GYN-CPD-PEDRO <pedroskzt@gmail.com>
2021-06-08 09:25:41 -0300

    Nome da URL alterado para o padrão da intranet.

--- Apps/subsTribut/urls.py
+++ Apps/subsTribut/urls.py
@@ -6,7 +6,7 @@ from django.urls import path
 from Apps.subsTribut.views.calcular_imposto import CalcularView
 
 urlpatterns = [
-    path('', CalcularView.as_view(), name='home'),
+    path('', CalcularView.as_view(), name='index_st'),
     path('', CalcularView.as_view(), name='calcular_imposto_individual'),
     path('numr_negociacao', CalcularView.as_view(), name='calcular_neg_vw'),
 ]
