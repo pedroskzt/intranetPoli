5eccd6a3061a0d9a930465047cadc3c4b87d409b
GYN-CPD-PEDRO <pedroskzt@gmail.com>
2021-06-10 16:51:14 -0300

    Removido método static não utilizado.

--- Apps/intranet/forms/form_links.py
+++ Apps/intranet/forms/form_links.py
@@ -3,11 +3,6 @@ from django import forms
 from Apps.intranet.models.links import Links
 
 
-def _campo_vazio(valor_campo, nome_campo, lista_de_erros):
-    if not valor_campo.strip():
-        lista_de_erros[nome_campo] = f'Este campo não pode estar vazio ou em branco.'
-
-
 class NovoLinkForms(forms.ModelForm):
     class Meta:
         model = Links
