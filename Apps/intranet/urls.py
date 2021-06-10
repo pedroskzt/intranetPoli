28ac44d93afcc776527f9d1b484103482205e7e0
GYN-CPD-PEDRO <pedroskzt@gmail.com>
2021-06-10 17:13:55 -0300

    Adicionado URL para página de cadastro de usuário

--- Apps/intranet/urls.py
+++ Apps/intranet/urls.py
@@ -6,13 +6,15 @@ from django.urls import path
 from Apps.intranet.views.gerenciar_intranet import painel_intranet, adicionar_link, editar_link, excluir_link
 from Apps.intranet.views.login import login, logout
 from Apps.intranet.views.pagina_inicial import index
+from Apps.intranet.views.usuario import adicionar_usuario
 
 urlpatterns = [
     path('', index, name='pagina_inicial'),
     path('login/next', login, name='login'),
     path('logout', logout, name='logout'),
     path('painel/intranet', painel_intranet, name='painel_intranet'),
-    path('painel/intranet/adicionar', adicionar_link, name='adicionar_link'),
+    path('painel/intranet/adicionar/link', adicionar_link, name='adicionar_link'),
+    path('painel/intranet/adicionar/usuario', adicionar_usuario, name='adicionar_usuario'),
     path('painel/intranet/editar/<int:link_id>', editar_link, name='editar_link'),
     path('painel/intranet/excluir/<int:link_id>', excluir_link, name='excluir_link'),
 ]
