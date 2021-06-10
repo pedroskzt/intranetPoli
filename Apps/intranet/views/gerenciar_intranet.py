33692fc1005f5b063c5f40c399bdf51f9863ad23
GYN-CPD-PEDRO <pedroskzt@gmail.com>
2021-06-10 16:52:26 -0300

    Adicionado Decorator que especifica quais Views deve exigir login para serem acessadas.

--- Apps/intranet/views/gerenciar_intranet.py
+++ Apps/intranet/views/gerenciar_intranet.py
@@ -1,17 +1,18 @@
 import os
 
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 
 from Apps.intranet.forms.form_links import NovoLinkForms, AtualizaLinkForms
 from Apps.intranet.models.links import Links
 from intranetPoli.settings import MEDIA_URL
 
-
+@login_required
 def painel_intranet(request):
     links = Links.objects.all()
     return render(request, 'intranet/painel/painel_intranet.html', context={'links': links})
 
-
+@login_required
 def adicionar_link(request):
     if request.method == 'POST':
         form = NovoLinkForms(request.POST, request.FILES)
@@ -25,7 +26,7 @@ def adicionar_link(request):
         form = NovoLinkForms()
     return render(request, 'intranet/painel/adicionar_link.html', context={'forms': form})
 
-
+@login_required
 def editar_link(request, link_id):
     if request.method == 'GET':
         link = get_object_or_404(Links, pk=link_id)
@@ -46,7 +47,7 @@ def editar_link(request, link_id):
     }
     return render(request, 'intranet/painel/editar_link.html', context=contexto)
 
-
+@login_required
 def excluir_link(request, link_id):
     # TODO: Adicionar modal de confirmação de exclusão
     link = get_object_or_404(Links, pk=link_id)
