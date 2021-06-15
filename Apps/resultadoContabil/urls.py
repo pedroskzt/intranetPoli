43e170a2021ff94aff946dbd3fc4e112c05af918
GYN-CPD-PEDRO <pedroskzt@gmail.com>
2021-06-15 17:14:14 -0300

    Iniciando desenvolvimento do site da contabilidade na intranet. - Cadastro de previsão de imposto - Atualização dos valores no B.I

---
+++ Apps/resultadoContabil/urls.py
@@ -0,0 +1,13 @@
+"""subsTribut URL Configuration
+
+"""
+from django.urls import path
+
+from Apps.intranet.views.gerenciar_intranet import painel_intranet, adicionar_link, editar_link, excluir_link
+from Apps.intranet.views.login import login, logout
+from Apps.resultadoContabil.views.index import index
+from Apps.intranet.views.usuario import adicionar_usuario
+
+urlpatterns = [
+    path('', index, name='index-contabil'),
+]
