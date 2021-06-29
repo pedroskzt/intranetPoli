a8c4f42e45a5c772220637afb47b4c4d009a1b70
GYN-CPD-PEDRO <pedroskzt@gmail.com>
2021-06-29 11:38:19 -0300

    Adicionado URLs da página de resultadoContabil.

--- Apps/resultadoContabil/urls.py
+++ Apps/resultadoContabil/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
 
-from Apps.intranet.views.gerenciar_intranet import painel_intranet, adicionar_link, editar_link, excluir_link
-from Apps.intranet.views.login import login, logout
-from Apps.resultadoContabil.views.index import index
-from Apps.intranet.views.usuario import adicionar_usuario
+from Apps.resultadoContabil.views.index import index, cadastro
 
 urlpatterns = [
-    path('', index, name='index-contabil'),
+    path('', index, name='index_contabil'),
+    path('consultar/', cadastro, name='consultar_cadastro'),
 ]
