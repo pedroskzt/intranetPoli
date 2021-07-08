ddccd7517e0ee1124ce40636f106edccc5f38cf0
GYN-CPD-PEDRO <pedroskzt@gmail.com>
2021-07-08 15:15:36 -0300

    Adicionado url para resquest do filtro de links da intranet.

--- Apps/intranet/urls.py
+++ Apps/intranet/urls.py
@@ -6,7 +6,7 @@ from django.urls import path
 from Apps.intranet.views.gerenciar_intranet import painel_intranet, adicionar_link, editar_link, excluir_link
 from Apps.intranet.views.log_models import log_models, ajax_log
 from Apps.intranet.views.login import login, logout
-from Apps.intranet.views.pagina_inicial import index
+from Apps.intranet.views.pagina_inicial import index, ajax_pesquisar_links
 from Apps.intranet.views.usuario import adicionar_usuario
 
 urlpatterns = [
@@ -20,4 +20,5 @@ urlpatterns = [
     path('painel/intranet/excluir/<int:link_id>', excluir_link, name='excluir_link'),
     path('painel/cpd/log/<str:model>/<str:tabela>/<str:changelist>', log_models, name='log_models'),
     path('painel/cpd/log/ajax', ajax_log, name='ajax_log'),
+    path('home/search/ajax', ajax_pesquisar_links, name='ajax_pesquisar_links'),
 ]
