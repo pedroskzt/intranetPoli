44e4d89364ac33c845710fbae4794faf70564213
GYN-CPD-PEDRO <pedroskzt@gmail.com>
2021-10-19 12:30:40 -0300

    View usuario.py movido para o app de controle de acesso. Adequação dos métodos para tal mudança. Adicionado métodos para editação de usuário, alteração e recuperação de senha e métodos de pesquisa de usuários.

---
+++ Apps/controleAcesso/views/usuario.py
@@ -0,0 +1,172 @@
+from django.contrib import messages
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth.forms import PasswordResetForm
+from django.contrib.auth.models import Group
+from django.contrib.auth.models import User
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import send_mail, BadHeaderError
+from django.db import connection
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
+
+from Apps.controleAcesso.forms.form_usuario import AtualizaUsuarioForms
+from Apps.controleAcesso.forms.form_usuario import NovoUsuarioForms
+
+
+def _group_check(user, permissions):
+    if user.is_authenticated:
+        permitir_acesso = True
+        for permission in permissions:
+            if user.has_perm(permission) is False:
+                permitir_acesso = False
+        if permitir_acesso:
+            return True
+    return False
+
+
+@user_passes_test(lambda user, permissions=('intranet.gerenciar_intranet',): _group_check(user, permissions),
+                  login_url='pagina_inicial', redirect_field_name=None)
+def adicionar_usuario(request):
+    """
+    View para adição de novos usuários na intranet.
+    GET:
+        Retorna a página de login em branco.
+    POST:
+        Valida os campos preenchidos, testa se a senha e confirmação de senha coincidem, se os campos obrigatorios foram
+        preenchidos, cria o cadastro do usuário no banco e retorna para página do painel da intranet.
+    :param request:
+    :return:
+    """
+    form = NovoUsuarioForms()
+    if request.method == 'POST':
+        form = NovoUsuarioForms(request.POST)
+        if form.is_valid():
+            new_user = User.objects.create_user(username=form.cleaned_data['usuario'],
+                                                email=form.cleaned_data['email'],
+                                                password=form.cleaned_data['senha'],
+                                                first_name=form.cleaned_data.get('nome'),
+                                                last_name=form.cleaned_data.get('sobrenome'))
+            new_user.is_staff = form.cleaned_data['adm_intranet']
+            new_user.groups.set(Group.objects.filter(pk__in=form.cleaned_data['groups']))
+            new_user.save()
+            return redirect('listar_usuarios')
+
+    return render(request, 'controleAcesso/usuarios/adicionar_usuario.html', context={'form': form})
+
+
+@login_required
+def alterar_senha(request):
+    """
+    View para alterar senha do usuário. Requer estar logado para acessar.
+    GET:
+        Retorna a página com formulário em branco de alteração de senha.
+    POST:
+        Verifica se a senha atual está correta, se a nova senha corresponde a confirmação da nova senha, valida se a
+        nova senha respeitas os requisitos e retorna para página de login caso esteja tudo certo.
+    :param request:
+    :return:
+    """
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # Importante, finaliza todas as sessões deste usuário.
+            messages.success(request, 'Senha alterada com sucesso.')
+            return redirect('alterar_senha')
+        else:
+            messages.error(request, 'Favor corrigir os erros destacados.')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'controleAcesso/usuarios/alterar_senha.html', {'form': form})
+
+
+def recuperar_senha(request):
+    if request.method == 'POST':
+        form = PasswordResetForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            usuario = User.objects.filter(email=email)
+            if usuario.exists():
+                usuario = usuario.get()
+                assunto = "Solicitação de recuperação de senha"
+                corpo_email = {
+                    "email": usuario.email,
+                    "domain": "localhost",
+                    "site_name": "Intranet Polipeças",
+                    "uid": urlsafe_base64_encode(force_bytes(usuario.pk)),
+                    "user": usuario,
+                    "token": default_token_generator.make_token(usuario),
+                    'protocol': 'http',
+                }
+                email = render_to_string("controleAcesso/usuarios/recuperar_senha.txt", corpo_email)
+                try:
+                    send_mail(subject=assunto, message=email, from_email='no-reply@polipecas.com.br',
+                              recipient_list=[usuario.email], fail_silently=False)
+                except BadHeaderError:
+                    return HttpResponse('Invalid header found.')
+            print("oi")
+            return redirect('recuperar_senha_enviado')
+    else:
+        form = PasswordResetForm()
+    return render(request, 'controleAcesso/usuarios/recuperar_senha.html', {'form': form})
+
+
+@user_passes_test(lambda user, permissions=('auth.change_user',): _group_check(user, permissions),
+                  login_url='pagina_inicial', redirect_field_name=None)
+def listar_usuarios(request):
+    usuarios = User.objects.all()
+    lista_usuarios = []
+    for usuario in usuarios:
+        usuario_id = usuario.pk
+        nome = usuario.get_full_name()
+        nome = usuario.username if nome == '' else nome
+        lista_usuarios.append({'id': usuario_id, 'nome': nome})
+    contexto = {'lista_usuarios': lista_usuarios}
+    return render(request, 'controleAcesso/usuarios/listar_usuarios.html', context=contexto)
+
+
+@user_passes_test(lambda user, permissions=('auth.change_user',): _group_check(user, permissions),
+                  login_url='pagina_inicial', redirect_field_name=None)
+def editar_usuario(request, usuario_id):
+    if request.method == 'GET':
+        usuario = get_object_or_404(User, pk=usuario_id)
+        form = AtualizaUsuarioForms(request.user, instance=usuario)
+
+    elif request.method == 'POST':
+        print(request.POST)
+        usuario = get_object_or_404(User, pk=usuario_id)
+        form = AtualizaUsuarioForms(request.user, request.POST, instance=usuario)
+        if request.user.is_authenticated and form.is_valid():
+            form.save()
+            return redirect('listar_usuarios')
+    contexto = {
+        'form': form,
+        'usuario_id': usuario_id
+    }
+    return render(request, 'controleAcesso/usuarios/editar_usuario.html', context=contexto)
+
+
+@user_passes_test(lambda user, permissions=('auth.change_user',): _group_check(user, permissions),
+                  login_url='pagina_inicial', redirect_field_name=None)
+def ajax_pesquisar_usuarios(request):
+    filtro = False
+    if request.method == 'GET':
+        usuarios_id = _query_select_usuarios(request.GET['pesquisar'])
+        filtro = {'usuarios_id': usuarios_id if usuarios_id else False}
+    return JsonResponse(filtro)
+
+
+def _query_select_usuarios(pesquisar):
+    with connection.cursor() as cursor:
+        cursor.execute("ALTER SESSION SET NLS_COMP=LINGUISTIC")
+        cursor.execute("ALTER SESSION SET NLS_SORT=BINARY_AI")
+        cursor.execute(f"SELECT id "
+                       f"FROM auth_user "
+                       f"where username like '%{pesquisar}%'")
+        retornoBanco = cursor.fetchall()
+    return [str(linha[0]) for linha in retornoBanco] if retornoBanco else False
