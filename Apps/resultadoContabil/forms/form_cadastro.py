adf5ed0bff0d87ff51615f265d35cb3e57c89b9c
GYN-CPD-PEDRO <pedroskzt@gmail.com>
2021-10-19 11:53:51 -0300

    Removido função não utilizada.

--- Apps/resultadoContabil/forms/form_cadastro.py
+++ Apps/resultadoContabil/forms/form_cadastro.py
@@ -1,19 +1,4 @@
 from django import forms
-from django.db import connections
-
-
-def _query_cadastro(mes, ano):
-    with connections['BI'].cursor() as cursor:
-        cursor.execute(
-            f'select t.valr_irpj_prev, t.valr_csll_prev '
-            f'from dw_resultado t '
-            f'where numr_ano = {ano} '
-            f'and t.numr_mes = {mes} '
-            f'and indr_tipo = 1 '
-            f'and id_empresa = 1')
-        retornoBanco = cursor.fetchone()
-        colunas = [col[0] for col in cursor.description]
-    return dict(zip(colunas, retornoBanco)) if retornoBanco else False
 
 
 class FormConsultaCadastro(forms.Form):
