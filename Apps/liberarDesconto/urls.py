b481b7d4799f3c75850236f87c1c2fead5f476a7
GYN-CPD-PEDRO <pedroskzt@gmail.com>
2021-11-01 17:47:16 -0300

    Criado url para acesso à página de liberação de desconto.

---
+++ Apps/liberarDesconto/urls.py
@@ -0,0 +1,11 @@
+"""subsTribut URL Configuration
+
+"""
+from django.contrib import admin
+from django.urls import path
+from Apps.liberarDesconto.views import liberar_desconto
+
+urlpatterns = [
+    path('', liberar_desconto, name='liberar_desconto'),
+
+]
