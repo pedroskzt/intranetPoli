cb3024d80db258fadc0403e98734f39e76590e48
GYN-CPD-PEDRO <pedroskzt@gmail.com>
2021-11-16 17:02:54 -0300

    Adicionado Decarator para verificar se o usuário possui as permissões necessárias para acessar cada View e adicionado mensagens de acesso negado em determinadas operações.

--- Apps/controleAcesso/views/gerenciarPermissoes.py
+++ Apps/controleAcesso/views/gerenciarPermissoes.py
@@ -1,18 +1,29 @@
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import Group, Permission, ContentType
 from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404
 
+from intranetPoli.decorators import verificar_permissoes
 
+
+@login_required
+@verificar_permissoes(permissoes_exigidas=['controleAcesso.view_controleacesso'])
 def gerenciar_permissoes(request):
     contexto = {'grupos': Group.objects.all()}
     if request.method == 'POST':
-        grupo = get_object_or_404(Group, pk=request.POST['selecionar_grupo'])
-        grupo.permissions.set(Permission.objects.filter(pk__in=request.POST.getlist('selecionar_permissoes')))
-        contexto['id_grupo'] = grupo.pk
+        if request.user.has_perm('controleAcesso.change_controleacesso'):
+            grupo = get_object_or_404(Group, pk=request.POST['selecionar_grupo'])
+            grupo.permissions.set(Permission.objects.filter(pk__in=request.POST.getlist('selecionar_permissoes')))
+            contexto['id_grupo'] = grupo.pk
+        else:
+            messages.error(request, f"Você não possui as permissões necessárias para esta operação. "
+                                    f"Caso precise deste acesso, entre em contato com o Departamento de Informática da Matriz.")
     return render(request, 'controleAcesso/permissoes/gerenciar_permissoes.html', context=contexto)
 
 
+@login_required
+@verificar_permissoes(permissoes_exigidas=['controleAcesso.view_controleacesso'])
 def ajax_permissoes_grupo(request):
     filtro = False
     if request.method == 'GET':
@@ -30,6 +41,8 @@ def ajax_permissoes_grupo(request):
     return JsonResponse(filtro)
 
 
+@login_required
+@verificar_permissoes(permissoes_exigidas=['controleAcesso.view_controleacesso'])
 def ajax_content_type(request):
     filtro = False
     if request.method == 'GET':
@@ -42,9 +55,10 @@ def ajax_content_type(request):
     return JsonResponse(filtro)
 
 
+@login_required
 def criar_permissao(request):
     contexto = {'grupos': Group.objects.all()}
-    if request.method == 'POST':
+    if request.method == 'POST' and request.user.has_perm('controleAcesso.add_controleacesso'):
         contexto['id_grupo'] = int(request.POST['id_grupo'])
         content_type = ContentType.objects.get(pk=request.POST['modelo_selecionado'])
         codinome = request.POST['permissao_nome'].lower().replace(' ', '_')
@@ -62,13 +76,17 @@ def criar_permissao(request):
                 Group.objects.get(pk=request.POST['id_grupo']).permissions.add(nova_permissao)
                 nome = str(nova_permissao).split(' | ')
                 contexto['permissao'] = {'id': nova_permissao.pk, 'nome': f'{nome[1]} | {nome[2]}'}
-
+    else:
+        messages.error(request, f"<p> Você não tem acesso à página ou operação que esta tentando acessar.</p>"
+                                f"<p> Caso precise de ter acesso a esta página, contate o Departamento de Informática da Matriz</p>",
+                       extra_tags='safe')
     return render(request, 'controleAcesso/permissoes/gerenciar_permissoes.html', context=contexto)
 
 
+@login_required
 def criar_grupo(request):
     contexto = {}
-    if request.method == 'POST':
+    if request.method == 'POST' and request.user.has_perm('controleAcesso.add_controleacesso'):
         if Group.objects.filter(name=request.POST['grupo_nome']).exists():
             messages.error(request,
                            f'Grupo "{Group.objects.filter(name=request.POST["grupo_nome"])[0]}" já exise!')
@@ -79,5 +97,9 @@ def criar_grupo(request):
                 messages.error(request, f'Erro ao criar grupo: {request.POST["grupo_nome"]}\n{err}')
             else:
                 contexto['id_grupo'] = int(novo_grupo.pk)
+    else:
+        messages.error(request, f"<p> Você não tem acesso à página ou operação que esta tentando acessar.</p>"
+                                f"<p> Caso precise de ter acesso a esta página, contate o Departamento de Informática da Matriz</p>",
+                       extra_tags='safe')
     contexto['grupos'] = Group.objects.all()
     return render(request, 'controleAcesso/permissoes/gerenciar_permissoes.html', context=contexto)
