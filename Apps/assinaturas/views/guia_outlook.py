a6079003cda49d680b5ac47c95f6a522182bf079
GYN-CPD-PEDRO <pedroskzt@gmail.com>
2021-12-23 11:50:27 -0300

    Código refatorado e alterado para o arquivo assinaturas_email.py na View tutoriais

--- Apps/assinaturas/views/guia_outlook.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-
-def guia_outlook(request):
-    pagina_selecionada = {"siteAssinatura": {"pagina_guias": 'active',
-                                             "guia_outlook": 'active'}}
-    return render(request, 'assinatura/guias/guia_outlook.html', pagina_selecionada)
