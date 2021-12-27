22dbd3a0dbbea21fdfb21bc34e24476da0a5f9ba
GYN-CPD-PEDRO <pedroskzt@gmail.com>
2021-12-27 17:57:53 -0300

    Alterado URL name da página inicial de resultado contabil de "index_contabil" para "consulta_cadastro_imposto"

--- Apps/resultadoContabil/urls.py
+++ Apps/resultadoContabil/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from Apps.resultadoContabil.views.index import index, cadastro, recalcular
+from Apps.resultadoContabil.views.resultadoContabil import consulta_cadastro_imposto, cadastro, recalcular
 
 urlpatterns = [
-    path('', index, name='index_contabil'),
+    path('', consulta_cadastro_imposto, name='consulta_cadastro_imposto'),
     path('consultar/', cadastro, name='consultar_cadastro'),
     path('recalcular/', recalcular, name='recalcular_bi'),
 ]
