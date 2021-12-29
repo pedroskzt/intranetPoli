acaf201d659da884834ea78d47f20526c8e6e66f
GYN-CPD-PEDRO <pedroskzt@gmail.com>
2021-12-29 08:58:45 -0300

    Criado View para exibir os catálogos na página inicial do APP de catálogos.

---
+++ Apps/catalogos/views/exibir_catalogos.py
@@ -0,0 +1,31 @@
+from django.db import connection
+from django.http import JsonResponse
+from django.shortcuts import render
+
+from Apps.catalogos.models.catalogos import Catalogos
+
+# Create your blah here.
+
+
+def _query_select_catalogos(pesquisar):
+    with connection.cursor() as cursor:
+        cursor.execute("ALTER SESSION SET NLS_COMP=LINGUISTIC")
+        cursor.execute("ALTER SESSION SET NLS_SORT=BINARY_AI")
+        cursor.execute(f"SELECT id "
+                       f"FROM catalogos_catalogos "
+                       f"where titulo like '%{pesquisar}%'")
+        retornoBanco = cursor.fetchall()
+    return [str(linha[0]) for linha in retornoBanco] if retornoBanco else False
+
+
+def exibir_catalogos(request):
+    catalogos = Catalogos.objects.filter(exibir=True)
+    return render(request, 'catalogos/catalogos.html', context={'catalogos': catalogos})
+
+
+def ajax_pesquisar_catalogos(request):
+    filtro = False
+    if request.is_ajax():
+        catalogo_id = _query_select_catalogos(request.GET['pesquisar'])
+        filtro = {'catalogo_id': catalogo_id if catalogo_id else False}
+    return JsonResponse(filtro)
