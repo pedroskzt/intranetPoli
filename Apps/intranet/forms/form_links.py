3c5cad6dcb12ce320f4344c4c673c5fa328798bd
GYN-CPD-PEDRO <pedroskzt@gmail.com>
2022-05-03 11:30:15 -0300

    Adicionado campo para selecionar as permissões necessárias para visualizar um link.

--- Apps/intranet/forms/form_links.py
+++ Apps/intranet/forms/form_links.py
@@ -6,7 +6,7 @@ from Apps.intranet.models.links import Links
 class NovoLinkForms(forms.ModelForm):
     class Meta:
         model = Links
-        fields = ('titulo', 'descricao', 'logo', 'url', 'exibir', 'requer_acesso', 'intranet')
+        fields = ('titulo', 'descricao', 'logo', 'url', 'exibir', 'requer_acesso', 'intranet', 'permissoes')
         labels = {
             'titulo': 'Título',
             'descricao': 'Descrição',
@@ -14,19 +14,21 @@ class NovoLinkForms(forms.ModelForm):
             'url': 'Link da Página',
             'exibir': 'Exibir?',
             'requer_acesso': 'Requer Acesso?',
-            'intranet': 'Intranet?'
+            'intranet': 'Intranet?',
+            'permissoes': 'Permissões para Exibição'
         }
         widgets = {
             'titulo': forms.TextInput(attrs={'placeholder': 'Título'}),
             'url': forms.TextInput(attrs={'placeholder': 'Link da Página'}),
             'descricao': forms.Textarea(attrs={'placeholder': 'Descrição'}),
+            'permissoes': forms.SelectMultiple(),
         }
 
 
 class AtualizaLinkForms(forms.ModelForm):
     class Meta:
         model = Links
-        fields = ('id', 'titulo', 'descricao', 'logo', 'url', 'exibir', 'requer_acesso', 'intranet')
+        fields = ('id', 'titulo', 'descricao', 'logo', 'url', 'exibir', 'requer_acesso', 'intranet', 'permissoes')
         labels = {
             'titulo': 'Título',
             'descricao': 'Descrição',
@@ -34,10 +36,12 @@ class AtualizaLinkForms(forms.ModelForm):
             'url': 'Link da Página',
             'exibir': 'Exibir?',
             'requer_acesso': 'Requer Acesso?',
-            'intranet': 'Intranet?'
+            'intranet': 'Intranet?',
+            'permissoes': 'Permissões para Exibição',
         }
         widgets = {
             'titulo': forms.TextInput(attrs={'placeholder': 'Título'}),
             'url': forms.TextInput(attrs={'placeholder': 'Link da Página'}),
-            'descricao': forms.Textarea(attrs={'placeholder': 'Descrição'})
+            'descricao': forms.Textarea(attrs={'placeholder': 'Descrição'}),
+            'permissoes': forms.SelectMultiple(),
         }
